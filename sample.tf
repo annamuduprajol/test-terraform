a11cc6aaa817610af5b11645daec4e5d9b13c021
Prajol <annamuduprajol@gmail.com>
2023-06-10 10:30:17 +0530

    this is my sample test terraform project

---
+++ sample.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+
+    region = "us-east-1"
+}
+
+resource "aws_ec2_instance" {
+
+
+    AMI = ""
+}
+
+resource "s3_bucket" {
+
+
+    jlksdklfjdskf
+
+    lksdjdfkldsajkldfs
+    lkasdjkldfjklasdfj
+
+    kjasdklfjlkasdf
+
+    lksadjfklajdsfkljadsf
+
+    lkasdfljlkasdf
+
+}
